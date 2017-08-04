ca5cc2672ba1b3a74ee54bb2278a9605f97abc76
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2017-08-04 18:26:50 +0300

    Add logging for found problems

--- src/main/kotlin/org/jetbrains/intellij/InspectionPluginExtension.kt
+++ src/main/kotlin/org/jetbrains/intellij/InspectionPluginExtension.kt
@@ -1,5 +1,7 @@
 package org.jetbrains.intellij
 
+import org.gradle.api.logging.LogLevel
+
 class InspectionPluginExtension(
         // Which properties are needed?
         // Number of IDEA version
@@ -7,5 +9,15 @@ class InspectionPluginExtension(
         // List of necessary IDEA plugin
         var ideaPlugins: List<String>,
         // List of necessary inspections to run
-        var inspectionClasses: List<String>
-)
+        var errorClasses: List<String>,
+        var warningClasses: List<String>,
+        var infoClasses: List<String>
+) {
+    val inspectionClasses get() = errorClasses + warningClasses + infoClasses
+
+    fun getLevel(inspectionClass: String) = when {
+        inspectionClass in errorClasses -> LogLevel.ERROR
+        inspectionClass in warningClasses -> LogLevel.WARN
+        else -> LogLevel.INFO
+    }
+}
--- src/main/kotlin/org/jetbrains/intellij/InspectionTask.kt
+++ src/main/kotlin/org/jetbrains/intellij/InspectionTask.kt
@@ -1,28 +1,46 @@
 package org.jetbrains.intellij
 
 import com.intellij.codeInspection.ProblemDescriptor
+import com.intellij.codeInspection.ProblemDescriptorUtil
+import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
-import org.gradle.api.DefaultTask
 import org.gradle.api.file.FileCollection
+import org.gradle.api.logging.LogLevel
 import org.gradle.api.tasks.SourceSet
+import org.gradle.api.tasks.SourceTask
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.TaskExecutionException
 import org.jetbrains.idea.inspections.InspectionRunner
-import org.slf4j.LoggerFactory
 import com.intellij.openapi.project.Project as IdeaProject
 import org.gradle.api.Project as GradleProject
 
-class InspectionTask : DefaultTask() {
-
-    private val log = LoggerFactory.getLogger(this::class.java)
+class InspectionTask : SourceTask() {
 
     lateinit var sourceSet: SourceSet
 
     private val sourceDirs: FileCollection
         get() = project.files(sourceSet.allSource.srcDirs.filter { !sourceSet.resources.contains(it) && it.exists() })
+
+    private fun ProblemDescriptor.level(default: LogLevel): LogLevel {
+        return when (highlightType) {
+            ProblemHighlightType.ERROR, ProblemHighlightType.GENERIC_ERROR -> LogLevel.ERROR
+            ProblemHighlightType.WEAK_WARNING -> LogLevel.WARN
+            ProblemHighlightType.INFORMATION -> LogLevel.INFO
+            else -> default
+        }
+    }
+
+    private fun ProblemDescriptor.render() =
+            StringUtil.replace(
+                    StringUtil.replace(
+                            descriptionTemplate,
+                            "#ref",
+                            psiElement?.let { ProblemDescriptorUtil.extractHighlightedText(this, it) } ?: ""
+                    ), " #loc ", " ")
 
     @TaskAction
     fun analyze() {
@@ -48,10 +66,14 @@ class InspectionTask : DefaultTask() {
                 }
                 results[inspectionClass] = inspectionResults
             }
-            // TODO: do something with results :)
+            for ((inspectionClass, problems) in results) {
+                for (problem in problems) {
+                    logger.log(problem.level(extension.getLevel(inspectionClass)), problem.render())
+                }
+            }
         }
         catch (e: Throwable) {
-            log.error(e.message)
+            logger.error(e.message)
             throw TaskExecutionException(this, Exception("Exception occurred in analyze task", e))
         }
     }
