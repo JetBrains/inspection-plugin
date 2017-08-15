dc6d964e485dc35565c5790465f6a9f4620aafb8
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2017-08-15 15:56:23 +0300

    Set correct source files for analysis

--- src/main/kotlin/org/jetbrains/intellij/InspectionTask.kt
+++ src/main/kotlin/org/jetbrains/intellij/InspectionTask.kt
@@ -1,12 +1,26 @@
 package org.jetbrains.intellij
 
-import org.gradle.api.tasks.SourceTask
-import org.gradle.api.tasks.TaskAction
-import org.gradle.api.tasks.TaskExecutionException
+import org.gradle.api.file.FileTree
+import org.gradle.api.tasks.*
 import org.jetbrains.idea.inspections.InspectionRunner
 import org.gradle.api.Project as GradleProject
 
 open class InspectionTask : SourceTask() {
+
+    private val sourceFileTree: FileTree by lazy {
+        @Suppress("UNCHECKED_CAST")
+        val sourceSets = project.properties["sourceSets"] as Iterable<SourceSet>
+        logger.info("Property sourceSets = " + sourceSets)
+        val sourceFileTree = sourceSets.fold<SourceSet, FileTree?>(null) { prevFileTree, sourceSet ->
+            val currentFileTree = sourceSet.allSource.asFileTree
+            prevFileTree?.plus(currentFileTree) ?: currentFileTree
+
+        }!!
+        logger.info("Analyze source files: " + sourceFileTree.map { it.name })
+        sourceFileTree
+    }
+
+    override fun getSource(): FileTree = sourceFileTree
 
     @TaskAction
     fun analyze() {
