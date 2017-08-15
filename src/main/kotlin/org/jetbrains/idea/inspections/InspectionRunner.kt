9a082b1b13fd5ffc903164ade9363dd375424602
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2017-08-15 15:39:39 +0300

    Move IDEA-bound logic to InspectionRunner

--- src/main/kotlin/org/jetbrains/idea/inspections/InspectionRunner.kt
+++ src/main/kotlin/org/jetbrains/idea/inspections/InspectionRunner.kt
@@ -1,22 +1,68 @@
 package org.jetbrains.idea.inspections
 
-import com.intellij.codeInspection.InspectionManager
-import com.intellij.codeInspection.LocalInspectionTool
-import com.intellij.codeInspection.ProblemDescriptor
-import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.codeInspection.*
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiManager
+import org.gradle.api.file.FileTree
+import org.gradle.api.logging.LogLevel
+import org.gradle.api.logging.Logger
+import org.jetbrains.intellij.InspectionPluginExtension
 
 class InspectionRunner(
-        inspectionClass: Class<LocalInspectionTool>
+        private vararg val inspectionClasses: String
 ) {
-    private val inspection: LocalInspectionTool = inspectionClass.newInstance()
+    private val ideaProjectManager = ProjectManager.getInstance()
+    private val ideaProject = ideaProjectManager.defaultProject // FIXME
+    private val psiManager = PsiManager.getInstance(ideaProject)
+    private val virtualFileManager = VirtualFileManager.getInstance()
 
-    @Suppress("UNCHECKED_CAST")
-    constructor(inspectionClassName: String): this(Class.forName(inspectionClassName) as Class<LocalInspectionTool>)
+    private fun ProblemDescriptor.level(default: LogLevel): LogLevel = when (highlightType) {
+        ProblemHighlightType.ERROR, ProblemHighlightType.GENERIC_ERROR -> LogLevel.ERROR
+        ProblemHighlightType.WEAK_WARNING -> LogLevel.WARN
+        ProblemHighlightType.INFORMATION -> LogLevel.INFO
+        else -> default
+    }
 
-    fun analyze(file: PsiFile): List<ProblemDescriptor> {
+    private fun ProblemDescriptor.render() =
+            StringUtil.replace(
+                    StringUtil.replace(
+                            descriptionTemplate,
+                            "#ref",
+                            psiElement?.let { ProblemDescriptorUtil.extractHighlightedText(this, it) } ?: ""
+                    ), " #loc ", " ")
+
+    fun analyzeTreeAndLogResults(tree: FileTree, extension: InspectionPluginExtension, logger: Logger) {
+        logger.info("Input classes: " + inspectionClasses.toList().toString())
+        val results = analyzeTree(tree)
+        for ((inspectionClass, problems) in results) {
+            for (problem in problems) {
+                logger.log(problem.level(extension.getLevel(inspectionClass)), problem.render())
+            }
+        }
+    }
+
+    private fun analyzeTree(tree: FileTree): Map<String, List<ProblemDescriptor>> {
+        val results: MutableMap<String, MutableList<ProblemDescriptor>> = mutableMapOf()
+        for (inspectionClass in inspectionClasses) {
+            @Suppress("UNCHECKED_CAST")
+            val inspectionTool = (Class.forName(inspectionClass) as Class<LocalInspectionTool>).newInstance()
+            val inspectionResults = mutableListOf<ProblemDescriptor>()
+            for (sourceFile in tree) {
+                val virtualFile = virtualFileManager.findFileByUrl(sourceFile.absolutePath) ?: continue
+                val psiFile = psiManager.findFile(virtualFile) ?: continue
+                inspectionResults += inspectionTool.analyze(psiFile)
+            }
+            results[inspectionClass] = inspectionResults
+        }
+        return results
+    }
+
+    private fun LocalInspectionTool.analyze(file: PsiFile): List<ProblemDescriptor> {
         val holder = ProblemsHolder(InspectionManager.getInstance(file.project), file, false)
-        val visitor = inspection.buildVisitor(holder, false)
+        val visitor = this.buildVisitor(holder, false)
         visitor.visitFile(file)
         return holder.results
     }
--- src/main/kotlin/org/jetbrains/intellij/InspectionTask.kt
+++ src/main/kotlin/org/jetbrains/intellij/InspectionTask.kt
@@ -1,64 +1,21 @@
 package org.jetbrains.intellij
 
-import com.intellij.codeInspection.ProblemDescriptor
-import com.intellij.codeInspection.ProblemDescriptorUtil
-import com.intellij.codeInspection.ProblemHighlightType
-import com.intellij.openapi.project.ProjectManager
-import com.intellij.openapi.util.text.StringUtil
-import com.intellij.openapi.vfs.VirtualFileManager
-import com.intellij.psi.PsiManager
-import org.gradle.api.logging.LogLevel
 import org.gradle.api.tasks.SourceTask
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.TaskExecutionException
 import org.jetbrains.idea.inspections.InspectionRunner
-import com.intellij.openapi.project.Project as IdeaProject
 import org.gradle.api.Project as GradleProject
 
 open class InspectionTask : SourceTask() {
-
-    private fun ProblemDescriptor.level(default: LogLevel): LogLevel {
-        return when (highlightType) {
-            ProblemHighlightType.ERROR, ProblemHighlightType.GENERIC_ERROR -> LogLevel.ERROR
-            ProblemHighlightType.WEAK_WARNING -> LogLevel.WARN
-            ProblemHighlightType.INFORMATION -> LogLevel.INFO
-            else -> default
-        }
-    }
-
-    private fun ProblemDescriptor.render() =
-            StringUtil.replace(
-                    StringUtil.replace(
-                            descriptionTemplate,
-                            "#ref",
-                            psiElement?.let { ProblemDescriptorUtil.extractHighlightedText(this, it) } ?: ""
-                    ), " #loc ", " ")
 
     @TaskAction
     fun analyze() {
         try {
             val extension = project.extensions.findByType(InspectionPluginExtension::class.java)
             val inspectionClasses = extension.inspectionClasses
-            val ideaProjectManager = ProjectManager.getInstance()
-            val ideaProject = ideaProjectManager.defaultProject // FIXME
-            val psiManager = PsiManager.getInstance(ideaProject)
-            val virtualFileManager = VirtualFileManager.getInstance()
-            val results: MutableMap<String, MutableList<ProblemDescriptor>> = mutableMapOf()
-            for (inspectionClass in inspectionClasses) {
-                val inspectionResults = mutableListOf<ProblemDescriptor>()
-                val runner = InspectionRunner(inspectionClass)
-                for (sourceFile in getSource()) {
-                    val virtualFile = virtualFileManager.findFileByUrl(sourceFile.absolutePath) ?: continue
-                    val psiFile = psiManager.findFile(virtualFile) ?: continue
-                    inspectionResults += runner.analyze(psiFile)
-                }
-                results[inspectionClass] = inspectionResults
-            }
-            for ((inspectionClass, problems) in results) {
-                for (problem in problems) {
-                    logger.log(problem.level(extension.getLevel(inspectionClass)), problem.render())
-                }
-            }
+
+            val runner = InspectionRunner(*inspectionClasses)
+            runner.analyzeTreeAndLogResults(getSource(), extension, logger)
         }
         catch (e: Throwable) {
             logger.error(e.message)
