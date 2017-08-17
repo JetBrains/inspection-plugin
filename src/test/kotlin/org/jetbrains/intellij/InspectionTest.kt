c8dfeb9ac26f1e7c704955b6a7b90fd4dd76c912
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2017-08-17 17:22:32 +0300

    Add simple configuration test

--- src/test/kotlin/org/jetbrains/intellij/InspectionTest.kt
+++ src/test/kotlin/org/jetbrains/intellij/InspectionTest.kt
@@ -17,9 +17,17 @@ class InspectionTest {
 
     private lateinit var buildFile: File
 
+    private lateinit var inspectionsFile: File
+
+    private lateinit var sourceFile: File
+
     @Before
     fun setup() {
         buildFile = testProjectDir.newFile("build.gradle")
+        testProjectDir.newFolder("config", "inspections")
+        inspectionsFile = testProjectDir.newFile("config/inspections/inspections.xml")
+        testProjectDir.newFolder("src")
+        sourceFile = testProjectDir.newFile("src/main.kt")
     }
 
     @Test
@@ -38,6 +46,57 @@ class InspectionTest {
 
         assertTrue(result.output.contains("Hello world!"))
         assertEquals(result.task(":helloWorld").outcome, SUCCESS)
+    }
+
+    @Test
+    fun testInspectionConfiguration() {
+        val buildFileContent =
+                """
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.1.4"
+    }
+}
+
+plugins {
+    id 'java'
+    id 'org.jetbrains.kotlin.jvm' version '1.1.4'
+    id 'org.jetbrains.intellij.inspections'
+}
+
+sourceSets {
+    main {
+        java {
+            srcDirs = ['src']
+        }
+    }
+}
+                """
+        writeFile(buildFile, buildFileContent)
+        val inspectionsFileContent =
+                """<?xml version="1.0" encoding="UTF-8" ?>
+<inspections>
+    <errors>    </errors>
+    <warnings>
+        <warning class = "org.jetbrains.kotlin.idea.intentions.ConvertToStringTemplateInspection"/>
+    </warnings>
+    <infos>    </infos>
+</inspections>
+                """
+        writeFile(inspectionsFile, inspectionsFileContent)
+        writeFile(sourceFile, "fun foo() = \"a\" + \"b\"")
+
+        val result = GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withArguments("--info", "--stacktrace", "inspectionsMain")
+                .withPluginClasspath()
+                .build()
+
+        println(result.output)
+        assertEquals(result.task(":inspectionsMain").outcome, SUCCESS)
     }
 
     private fun writeFile(destination: File, content: String) {
