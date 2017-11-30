00cf592e334e9f2e76b59657ff7224fc4f78fab6
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2017-11-30 17:15:16 +0300

    Add bintray plugin classpath to root project to fix CCE in upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,11 @@ buildscript {
         mavenCentral()
         maven { setUrl("https://dl.bintray.com/kotlin/kotlin-dev") }
         mavenLocal()
+        maven { setUrl("https://plugins.gradle.org/m2/") }
     }
     dependencies {
         classpath("org.jetbrains.intellij.plugins:inspection-plugin:0.1.1-SNAPSHOT")
+        classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.0")
     }
 }
 
