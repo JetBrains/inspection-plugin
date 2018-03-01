cc17a8ddcfa504feb94d92dc09aea5e7471ce58a
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2018-03-01 12:46:54 +0300

    HTML report generator: add test for info / unused inspections

--- plugin/testData/inspection/htmlOutput/My.kt
+++ plugin/testData/inspection/htmlOutput/My.kt
@@ -1,5 +1,7 @@
 // warning: org.jetbrains.kotlin.idea.inspections.DataClassPrivateConstructorInspection
 // error: org.jetbrains.kotlin.idea.inspections.CanBeValInspection
+// info: org.jetbrains.kotlin.idea.intentions.FoldInitializerAndIfToElvisInspection
+// warning: org.jetbrains.kotlin.idea.inspections.UnusedSymbolInspection
 // htmlReport = true
 // maxErrors = 1000
 
@@ -10,4 +12,15 @@ fun canBeVal(): Int {
     var y = x * 2
     var z = "98" + y
     return z.toInt()
+}
+
+fun foldToElvis(arg: Int?): Int {
+    val n = arg
+    if (n == null) return -1
+    return n
+}
+
+class Unused {
+    val s: String get() = ""
+    fun bar(s: String) = s
 }
