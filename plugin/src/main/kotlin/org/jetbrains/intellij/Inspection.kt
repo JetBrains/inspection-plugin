8af2b06e95333ebbb8cbeb686e61ee7c70e50981
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2018-03-13 13:54:51 +0300

    Plugin / runner: advance to version 0.1.4-SNAPSHOT

--- plugin/src/main/kotlin/org/jetbrains/intellij/Inspection.kt
+++ plugin/src/main/kotlin/org/jetbrains/intellij/Inspection.kt
@@ -23,7 +23,7 @@ open class Inspection : SourceTask(), VerificationTask, Reporting<CheckstyleRepo
 
     companion object {
         // TODO: take the same version as plugin
-        const val runnerVersion = "0.1.3"
+        const val runnerVersion = "0.1.4-SNAPSHOT"
     }
 
     object ClassloaderContainer {
