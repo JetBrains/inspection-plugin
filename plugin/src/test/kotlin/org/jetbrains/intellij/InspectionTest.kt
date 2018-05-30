dbd1c38c5924f22dad4e543b25b33021117ea68d
Mikhail Glukhikh <glukhikh@mail.ru>
2018-05-30 19:48:24 +0300

    Minor: change test comment (delay 5 s)

--- plugin/src/test/kotlin/org/jetbrains/intellij/InspectionTest.kt
+++ plugin/src/test/kotlin/org/jetbrains/intellij/InspectionTest.kt
@@ -90,7 +90,8 @@ class InspectionTest {
             vararg expectedDiagnostics: String
     ) {
         // Delay to solve DaemonDisappearedException problem
-        // TODO: remove it and solve the problem via extending daemon timeout
+        // Reason: IDEA performs System.exit on shutdown()
+        // TODO: remove it and solve the problem (run in separate process?)
         Thread.sleep(5000)
         val result = try {
             GradleRunner.create()
