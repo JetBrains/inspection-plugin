8fb94c8846d3aa1d50e9db69dbdf592fa7531b3e
Sergei Vorobyov <hacker.mad.cat@gmail.com>
2018-08-29 20:32:29 +0300

    update plugin configuration
    unify configuration location
    base cache dir moved to home

--- plugin/src/main/kotlin/org/jetbrains/intellij/tasks/DownloadKotlinPluginTask.kt
+++ plugin/src/main/kotlin/org/jetbrains/intellij/tasks/DownloadKotlinPluginTask.kt
@@ -2,10 +2,9 @@ package org.jetbrains.intellij.tasks
 
 import org.gradle.api.internal.ConventionTask
 import org.gradle.api.tasks.*
-import org.jetbrains.intellij.ExceptionHandler
-import org.jetbrains.intellij.InspectionPlugin
 import org.jetbrains.intellij.extensions.InspectionPluginExtension
-import org.jetbrains.intellij.plugins.KotlinPlugin
+import org.jetbrains.intellij.configurations.*
+import org.jetbrains.intellij.exception
 import org.jetbrains.intellij.utils.Downloader
 import java.io.File
 
@@ -14,7 +13,7 @@ open class DownloadKotlinPluginTask : ConventionTask() {
 
     @get:Input
     val ideaVersion: String
-        get() = InspectionPlugin.ideaVersion(extension.idea.version)
+        get() = ideaVersion(extension.idea.version)
 
     @get:Input
     @get:Optional
@@ -24,12 +23,12 @@ open class DownloadKotlinPluginTask : ConventionTask() {
     @get:Input
     @get:Optional
     val location: String?
-        get() = extension.plugins.kotlin.location ?: InspectionPlugin.kotlinPluginLocation(version, ideaVersion)
+        get() = extension.plugins.kotlin.location ?: kotlinPluginLocation(version, ideaVersion)
 
     @get:OutputDirectory
     @get:Optional
     val archiveDirectory: File?
-        get() = location?.let { InspectionPlugin.kotlinPluginArchiveDirectory(it) }
+        get() = location?.let { kotlinPluginArchiveDirectory(it) }
 
     @Suppress("unused")
     @TaskAction
@@ -40,9 +39,9 @@ open class DownloadKotlinPluginTask : ConventionTask() {
             logger.info("InspectionPlugin: Using kotlin plugin inherit from idea. No downloading needed.")
             return
         }
-        if (version == null) ExceptionHandler.exception(this, "Expected version for kotlin plugin $location")
-        if (location == null) ExceptionHandler.exception(this, "Expected url for kotlin plugin $version")
-        KotlinPlugin.checkCompatibility(this, version, ideaVersion)
+        if (version == null) exception(this, "Expected version for kotlin plugin $location")
+        if (location == null) exception(this, "Expected url for kotlin plugin $version")
+        checkCompatibility(this, version, ideaVersion)
         Downloader(logger).download(location, archiveDirectory!!)
     }
 
