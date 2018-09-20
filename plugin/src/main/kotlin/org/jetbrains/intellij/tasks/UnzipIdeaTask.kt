88f3a33c694ea2243e61de1a4cc5025e2bc93ec5
Sergei Vorobyov <hacker.mad.cat@gmail.com>
2018-09-20 15:55:23 +0300

    Fix check & download for minor idea versions

--- plugin/src/main/kotlin/org/jetbrains/intellij/tasks/UnzipIdeaTask.kt
+++ plugin/src/main/kotlin/org/jetbrains/intellij/tasks/UnzipIdeaTask.kt
@@ -2,6 +2,7 @@ package org.jetbrains.intellij.tasks
 
 import org.gradle.api.internal.ConventionTask
 import org.gradle.api.tasks.*
+import org.gradle.internal.hash.HashUtil
 import org.jetbrains.intellij.configurations.*
 import org.jetbrains.intellij.extensions.InspectionPluginExtension
 import org.jetbrains.intellij.utils.*
@@ -33,7 +34,7 @@ open class UnzipIdeaTask : ConventionTask() {
     @Suppress("unused")
     @TaskAction
     fun apply() {
-        UpToDateChecker(ideaVersion, isTempDirInHome).apply {
+        UpToDateChecker(HashUtil.sha256(archive).asHexString(), isTempDirInHome).apply {
             onUpToDate {
                 logger.info("InspectionPlugin: No unzipping needed.")
             }
