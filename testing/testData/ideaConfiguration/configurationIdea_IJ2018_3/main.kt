add5606e94f712be472a853822f725b78be45bf1
Mikhail Glukhikh <glukhikh@mail.ru>
2018-12-13 16:41:00 +0300

    Add test for IDEA 2018.3 configuration

---
+++ testing/testData/ideaConfiguration/configurationIdea_IJ2018_3/main.kt
@@ -0,0 +1,9 @@
+// warning: org.jetbrains.kotlin.idea.inspections.RedundantVisibilityModifierInspection
+// idea.version = ideaIC:2018.3
+
+public val x = 42
+
+public val y = 13
+
+// :4:1: Redundant visibility modifier
+// :6:1: Redundant visibility modifier
