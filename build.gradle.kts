bb729578506bde59ce9aae231378ad39a6d600d1
Mikhail Glukhikh <glukhikh@mail.ru>
2019-09-18 13:38:00 +0300

    Comment about publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,4 +9,9 @@ buildscript {
         mavenCentral()
         maven { setUrl("https://plugins.gradle.org/m2/") }
     }
+
+    // NB: to publish, add classpath of bintray plugin here
+    // and remove its versions from children' build.gradle.kts.
+    // However, classpath here breaks sample compilation
+    // so I do not include it
 }
